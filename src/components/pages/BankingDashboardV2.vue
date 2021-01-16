366c06bd34ff54b9f854315d28f8a4b2c16a0eac
Sacha STAFYNIAK <sacha.stafyniak@gmail.com>
2021-01-16 18:59:11 +0100

    feat: banking dashboard

--- src/components/pages/BankingDashboardV2.vue
+++ src/components/pages/BankingDashboardV2.vue
@@ -1,3 +1,112 @@
+<script setup lang="ts">
+import { themeColors } from '/@src/composition/state/themeColors'
+
+function generateDayWiseTimeSeries(s: number, count: number) {
+        var values = [[
+            4, 3, 10, 9, 29, 19, 25, 9, 12, 7, 19, 5, 13, 9, 17, 2, 7, 5
+        ], [
+            2, 3, 8, 7, 22, 16, 23, 7, 11, 5, 12, 5, 10, 4, 15, 2, 6, 2
+        ]];
+        var i = 0;
+        var series = [];
+        var x = new Date("11 Nov 2020").getTime();
+        while (i < count) {
+            series.push([x, values[s][i]]);
+            x += 86400000;
+            i++;
+        }
+        return series;
+    }
+
+//Timeline Chart
+    const options = {
+        chart: {
+            type: "area",
+            height: 220,
+            foreColor: "#999",
+            stacked: true,
+            toolbar: {
+                show: false
+            },
+            dropShadow: {
+                enabled: true,
+                enabledSeries: [0],
+                top: -2,
+                left: 2,
+                blur: 5,
+                opacity: 0.06
+            }
+        },
+        colors: [themeColors.accent, themeColors.orange, themeColors.orange],
+        stroke: {
+            curve: "smooth",
+            width: 3
+        },
+        title: {
+            text: '',
+            align: 'left'
+        },
+        dataLabels: {
+            enabled: false
+        },
+        series: [{
+            name: 'Cash Expenses',
+            data: generateDayWiseTimeSeries(0, 18)
+        }, {
+            name: 'Card Expenses',
+            data: generateDayWiseTimeSeries(1, 18)
+        }],
+        markers: {
+            size: 0,
+            strokeColor: "#fff",
+            strokeWidth: 3,
+            strokeOpacity: 1,
+            fillOpacity: 1,
+            hover: {
+                size: 6
+            }
+        },
+        xaxis: {
+            type: "datetime",
+            axisBorder: {
+                show: false
+            },
+            axisTicks: {
+                show: false
+            }
+        },
+        yaxis: {
+            labels: {
+                offsetX: 0,
+                offsetY: -5
+            },
+            tooltip: {
+                enabled: true
+            }
+        },
+        grid: {
+            show: false,
+            padding: {
+                left: -5,
+                right: 5
+            }
+        },
+        tooltip: {
+            x: {
+                format: "dd MMM yyyy"
+            },
+        },
+        legend: {
+            position: 'top',
+            horizontalAlign: 'left'
+        },
+        fill: {
+            type: "solid",
+            fillOpacity: 0.7
+        }
+    };
+</script>
+
 <template>
   <!--Banking Dashboard V2-->
   <div class="banking-dashboard banking-dashboard-v2">
@@ -265,7 +374,13 @@
               <div class="column is-8">
                 <div class="chart-wrapper">
                   <a class="action-link">All Reports</a>
-                  <div id="timeline-chart"></div>
+                  <apexchart
+                    id="timeline-chart"
+                    :height="options.chart.height"
+                    :type="options.chart.type"
+                    :series="options.series"
+                    :options="options"
+                  ></apexchart>
                 </div>
               </div>
             </div>
