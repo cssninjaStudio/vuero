c74b9e1d6bff0dc819fcc7bab28da940e1e60bae
Sacha STAFYNIAK <sacha.stafyniak@gmail.com>
2021-02-15 20:33:39 +0100

    fix: onboarding welcome

--- src/components/pages/layouts/onboarding/OnboardingStandard.vue
+++ src/components/pages/layouts/onboarding/OnboardingStandard.vue
@@ -85,7 +85,7 @@
 </template>
 
 <style lang="scss">
-@import '../../assets/scss/abstracts/_variables.scss';
-@import '../../assets/scss/abstracts/_mixins.scss';
-@import '../../assets/scss/pages/generic/_onboarding.scss';
+@import '../../../../assets/scss/abstracts/_variables.scss';
+@import '../../../../assets/scss/abstracts/_mixins.scss';
+@import '../../../../assets/scss/pages/generic/_onboarding.scss';
 </style>
