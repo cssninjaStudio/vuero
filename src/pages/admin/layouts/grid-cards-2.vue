b0a271396f73b18b601d6f55d5826b53eb644b1c
Driss Chelouati <driss.chelouati@gmail.com>
2021-02-17 00:01:40 +0100

    pushing grids and grid data (WIP)

--- src/pages/admin/layouts/grid-cards-2.vue
+++ src/pages/admin/layouts/grid-cards-2.vue
@@ -40,68 +40,6 @@ useHead({
       <Toolbar />
     </div>
 
-    <div class="card-grid-toolbar">
-      <div class="control has-icon">
-        <input
-          class="input custom-text-filter"
-          placeholder="Search..."
-          data-filter-target=".column"
-        />
-        <div class="form-icon">
-          <i class="iconify" data-icon="feather:search"></i>
-        </div>
-      </div>
-
-      <div class="buttons">
-        <div class="field h-hidden-mobile">
-          <div class="control">
-            <div class="h-select">
-              <div class="select-box">
-                <span>All Projects</span>
-              </div>
-              <div class="select-icon">
-                <i class="iconify" data-icon="feather:chevron-down"></i>
-              </div>
-              <div class="select-drop has-slimscroll-sm">
-                <div class="drop-inner">
-                  <div class="option-row">
-                    <input type="radio" name="grid_filter" />
-                    <div class="option-meta">
-                      <span>Web Apps</span>
-                    </div>
-                  </div>
-                  <div class="option-row">
-                    <input type="radio" name="grid_filter" />
-                    <div class="option-meta">
-                      <span>Mobile Apps</span>
-                    </div>
-                  </div>
-                  <div class="option-row">
-                    <input type="radio" name="grid_filter" />
-                    <div class="option-meta">
-                      <span>Dashboards</span>
-                    </div>
-                  </div>
-                  <div class="option-row">
-                    <input type="radio" name="grid_filter" />
-                    <div class="option-meta">
-                      <span>Landing Pages</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <button class="button v-button is-primary is-raised">
-          <span class="icon">
-            <i class="fas fa-plus"></i>
-          </span>
-          <span>New Project</span>
-        </button>
-      </div>
-    </div>
-
     <div class="page-content-inner">
       <CardsGridV2 />
     </div>
