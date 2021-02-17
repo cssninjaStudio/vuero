d59f77b3629e2f572010b3d5c880521f90ef1389
Sacha STAFYNIAK <sacha.stafyniak@gmail.com>
2021-02-17 18:14:18 +0100

    chore: update i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,25 +1,12 @@
 import { useStorage } from '@vueuse/core'
 import { createI18n } from 'vue-i18n'
 
-// import i18n locales
-import en from '/@src/locales/en.json'
-import zhCN from '/@src/locales/zh-CN.json'
-import fr from '/@src/locales/fr.json'
-import es from '/@src/locales/es.json'
-import de from '/@src/locales/de.json'
-import esMx from '/@src/locales/es-MX.json'
+import messages from '@intlify/vite-plugin-vue-i18n/messages'
 
 const defaultLocale = useStorage('locale', navigator?.language || 'en')
 const i18n = createI18n({
   locale: defaultLocale.value,
-  messages: {
-    en,
-    'zh-CN': zhCN,
-    fr,
-    es,
-    de,
-    'es-MX': esMx,
-  },
+  messages,
 })
 
 export default i18n
