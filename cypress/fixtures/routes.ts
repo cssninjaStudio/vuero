9c09294589ae1f4c1064dbbd68ffe14b12c304f7
Driss Chelouati <driss.chelouati@gmail.com>
2021-02-18 23:38:04 +0100

    updated cypress fixtures

--- cypress/fixtures/routes.ts
+++ cypress/fixtures/routes.ts
@@ -6,31 +6,37 @@ export const minimal = [
     name: 'Homepage',
     path: '/',
     prefix: 'minimal',
+    displayOrder: 8,
   },
   {
     name: 'Messaging V2',
     path: '/messaging-v2',
     prefix: 'minimal',
+    displayOrder: 1,
   },
   {
     name: 'Status',
     path: '/status',
     prefix: 'minimal',
+    displayOrder: 3,
   },
   {
     name: 'Wizard V1',
     path: '/wizard-v1',
     prefix: 'minimal',
+    displayOrder: 2,
   },
   {
     name: 'Inbox',
     path: '/inbox',
     prefix: 'minimal',
+    displayOrder: 0,
   },
   {
     name: 'Error 1',
     path: '/error/page-1',
     prefix: 'minimal',
+    displayOrder: 4,
   },
   {
     name: 'Error 2',
@@ -41,16 +47,19 @@ export const minimal = [
     name: 'Error 3',
     path: '/error/page-3',
     prefix: 'minimal',
+    displayOrder: 5,
   },
   {
     name: 'Error 4',
     path: '/error/page-4',
     prefix: 'minimal',
+    displayOrder: 6,
   },
   {
     name: 'Error 5',
     path: '/error/page-5',
     prefix: 'minimal',
+    displayOrder: 7,
   },
 ]
 
@@ -59,31 +68,37 @@ export const auth = [
     name: 'Login 1',
     path: '/auth/login-1',
     prefix: 'auth',
+    displayOrder: 0,
   },
   {
     name: 'Login 2',
     path: '/auth/login-2',
     prefix: 'auth',
+    displayOrder: 1,
   },
   {
     name: 'Login 3',
     path: '/auth/login-3',
     prefix: 'auth',
+    displayOrder: 2,
   },
   {
     name: 'Signup 1',
     path: '/auth/signup-1',
     prefix: 'auth',
+    displayOrder: 3,
   },
   {
     name: 'Signup 2',
     path: '/auth/signup-2',
     prefix: 'auth',
+    displayOrder: 4,
   },
   {
     name: 'Signup 3',
     path: '/auth/signup-3',
     prefix: 'auth',
+    displayOrder: 5,
   },
 ]
 
@@ -91,210 +106,262 @@ const layouts = [
   {
     name: 'Action Page V1',
     path: '/action-page-1',
+    displayOrder: 33,
   },
   {
     name: 'Action Page V2',
     path: '/action-page-2',
+    displayOrder: 34,
   },
   {
     name: 'Form V1',
     path: '/form-layouts-1',
+    displayOrder: 48,
   },
   {
     name: 'Form V2',
     path: '/form-layouts-2',
+    displayOrder: 49,
   },
   {
     name: 'Form V3',
     path: '/form-layouts-3',
+    displayOrder: 50,
   },
   {
     name: 'Form V4',
     path: '/form-layouts-4',
+    displayOrder: 51,
   },
   {
     name: 'Grid Card V1',
     path: '/grid-cards-1',
+    displayOrder: 11,
   },
   {
     name: 'Grid Card V2',
     path: '/grid-cards-2',
+    displayOrder: 12,
   },
   {
     name: 'Grid Card V3',
     path: '/grid-cards-3',
+    displayOrder: 13,
   },
   {
     name: 'Grid Card V4',
     path: '/grid-cards-4',
+    displayOrder: 14,
   },
   {
     name: 'Grid Tiles V1',
     path: '/grid-tiles-1',
+    displayOrder: 15,
   },
   {
     name: 'Grid Tiles V2',
     path: '/grid-tiles-2',
+    displayOrder: 16,
   },
   {
     name: 'Grid Tiles V3',
     path: '/grid-tiles-3',
+    displayOrder: 17,
   },
   {
     name: 'Grid Users V1',
     path: '/grid-users-1',
+    displayOrder: 18,
   },
   {
     name: 'Grid Users V2',
     path: '/grid-users-2',
+    displayOrder: 19,
   },
   {
     name: 'Grid Users V3',
     path: '/grid-users-3',
+    displayOrder: 20,
   },
   {
     name: 'Grid Users V4',
     path: '/grid-users-4',
+    displayOrder: 21,
   },
   {
     name: 'Onboarding Standard',
     path: '/onboarding-welcome',
+    displayOrder: 32,
   },
   {
     name: 'Kanban Board',
     path: '/kanban-board',
+    displayOrder: 39,
   },
   {
     name: 'List Datatable V1',
     path: '/list-datatable-1',
+    displayOrder: 7,
   },
   {
     name: 'List Datatable V2',
     path: '/list-datatable-2',
+    displayOrder: 8,
   },
   {
     name: 'List Datatable V3',
     path: '/list-datatable-3',
+    displayOrder: 9,
   },
   {
     name: 'List Datatable V4',
     path: '/list-datatable-4',
+    displayOrder: 10,
   },
   {
     name: 'List Flex V1',
     path: '/list-flex-1',
+    displayOrder: 4,
   },
   {
     name: 'List Flex V2',
     path: '/list-flex-2',
+    displayOrder: 5,
   },
   {
     name: 'List Flex V3',
     path: '/list-flex-3',
+    displayOrder: 6,
   },
   {
     name: 'Homepage (List View V1)',
     path: '/',
+    displayOrder: 0,
   },
   {
     name: 'List View V2',
     path: '/list-view-2',
+    displayOrder: 1,
   },
   {
     name: 'List View V3',
     path: '/list-view-3',
+    displayOrder: 2,
   },
   {
     name: 'List View V4',
     path: '/list-view-4',
+    displayOrder: 3,
   },
   {
     name: 'Onboarding Page V1',
     path: '/onboarding-page-1',
+    displayOrder: 43,
   },
   {
     name: 'Onboarding Page V2',
     path: '/onboarding-page-2',
+    displayOrder: 44,
   },
   {
     name: 'Onboarding Page V3',
     path: '/onboarding-page-3',
+    displayOrder: 45,
   },
   {
     name: 'Onboarding Page V4',
     path: '/onboarding-page-4',
+    displayOrder: 46,
   },
   {
     name: 'Onboarding Page V5',
     path: '/onboarding-page-5',
+    displayOrder: 47,
   },
   {
     name: 'Profile Edit',
     path: '/profile-edit',
+    displayOrder: 23,
   },
   {
     name: 'Profile Edit Experience',
     path: '/profile-edit/experience',
+    displayOrder: 24,
   },
   {
     name: 'Profile Edit Skills',
     path: '/profile-edit/skills',
+    displayOrder: 25,
   },
   {
     name: 'Profile Edit Settings',
     path: '/profile-edit/settings',
+    displayOrder: 26,
   },
   {
     name: 'Profile Notifications',
     path: '/profile-notifications',
+    displayOrder: 27,
   },
   {
     name: 'Profile Settings',
     path: '/profile-settings',
+    displayOrder: 28,
   },
   {
     name: 'Profile View',
     path: '/profile-view',
+    displayOrder: 22,
   },
   {
     name: 'Projects Details',
     path: '/projects-details',
+    displayOrder: 38,
   },
   {
     name: 'Projects List V1',
     path: '/projects-projects-1',
+    displayOrder: 35,
   },
   {
     name: 'Projects List V2',
     path: '/projects-projects-2',
+    displayOrder: 36,
   },
   {
     name: 'Projects List V3',
     path: '/projects-projects-3',
+    displayOrder: 37,
   },
   {
     name: 'SaaS Billing',
     path: '/saas-billing',
+    displayOrder: 31,
   },
   {
     name: 'Search Empty',
     path: '/search-empty',
+    displayOrder: 30,
   },
   {
     name: 'Search Results',
     path: '/search-results',
+    displayOrder: 29,
   },
   {
     name: 'Utility Account Confirm',
     path: '/utility-account-confirm',
+    displayOrder: 40,
   },
   {
     name: 'Utility Invoice',
     path: '/utility-invoice',
+    displayOrder: 42,
   },
   {
     name: 'Utility Promotion',
     path: '/utility-promotion',
+    displayOrder: 41,
   },
 ]
 
@@ -302,98 +369,122 @@ const dashboards = [
   {
     name: 'Food Delivery',
     path: '/food-delivery',
+    displayOrder: 18,
   },
   {
     name: 'Banking V1',
     path: '/banking-1',
+    displayOrder: 7,
   },
   {
     name: 'Banking V2',
     path: '/banking-2',
+    displayOrder: 8,
   },
   {
     name: 'Banking V3',
     path: '/banking-3',
+    displayOrder: 9,
   },
   {
     name: 'Flights',
     path: '/flights',
+    displayOrder: 10,
   },
   {
     name: 'Company',
     path: '/company',
+    displayOrder: 11,
   },
   {
     name: 'Human Ressources',
     path: '/human-ressources',
+    displayOrder: 12,
   },
   {
     name: 'Charts Apex',
     path: '/charts-apex',
+    displayOrder: 19,
   },
   {
     name: 'Charts BillboardsJS',
     path: '/charts-billboardsjs',
+    displayOrder: 20,
   },
   {
     name: 'Ecommerce V1',
     path: '/ecommerce-1',
+    displayOrder: 17,
   },
   {
     name: 'Analytics',
     path: '/analytics',
+    displayOrder: 4,
   },
   {
     name: 'Stocks',
     path: '/stocks',
+    displayOrder: 5,
   },
   {
     name: 'Sales',
     path: '/sales',
+    displayOrder: 6,
   },
   {
     name: 'Influencer',
     path: '/influencer',
+    displayOrder: 13,
   },
   {
     name: 'Hobbies',
     path: '/hobbies',
+    displayOrder: 14,
   },
   {
     name: 'Health',
     path: '/health',
+    displayOrder: 15,
   },
   {
     name: 'Writer',
     path: '/writer',
+    displayOrder: 16,
   },
   {
     name: 'Homepage (Personal V1)',
     path: '/',
+    displayOrder: 0,
   },
   {
     name: 'Personal V2',
     path: '/personal-2',
+    displayOrder: 1,
   },
   {
     name: 'Personal V3',
     path: '/personal-3',
+    displayOrder: 2,
   },
   {
     name: 'Widgets Creative',
     path: '/widgets-sample-creative',
+    displayOrder: 22,
   },
   {
     name: 'Widgets List',
     path: '/widgets-sample-list',
+    displayOrder: 23,
   },
   {
     name: 'Widgets Stats',
     path: '/widgets-sample-stats',
+    displayOrder: 24,
   },
   {
     name: 'Widgets UI',
     path: '/widgets-sample-ui',
+    displayOrder: 21,
   },
 ]
 
