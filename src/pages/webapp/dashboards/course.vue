b51ca988c098a905407c29977fb1450d09c651b0
Driss Chelouati <driss.chelouati@gmail.com>
2021-02-26 15:48:34 +0100

    pushing course dashboard

---
+++ src/pages/webapp/dashboards/course.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useHead } from '@vueuse/head'
+
+useHead({
+  title: 'Dashboards Business 4 - WebApp - Vuero',
+})
+</script>
+
+<template>
+  <div class="page-content-inner">
+    <CourseDashboard class="has-webapp-spacing" />
+  </div>
+</template>
