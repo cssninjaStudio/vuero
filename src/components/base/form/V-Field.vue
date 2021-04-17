9a2985d2bfd284024e664a9121ad854baa3afc74
Sacha STAFYNIAK <sacha.stafyniak@gmail.com>
2021-04-17 20:36:12 +0200

    feat(v-field): add label and horizontal props

--- src/components/base/form/V-Field.vue
+++ src/components/base/form/V-Field.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
 import { defineProps } from 'vue'
 
-defineProps({
+const props = defineProps({
+  label: {
+    type: String,
+    default: undefined,
+  },
   addons: {
     type: Boolean,
     default: false,
@@ -14,6 +18,10 @@ defineProps({
     type: Boolean,
     default: false,
   },
+  horizontal: {
+    type: Boolean,
+    default: false,
+  },
 })
 </script>
 
@@ -21,11 +29,29 @@ defineProps({
   <div
     class="field"
     :class="[
-      addons && 'has-addons',
-      textaddon && 'has-textarea-addon',
-      grouped && 'is-grouped',
+      props.addons && 'has-addons',
+      props.textaddon && 'has-textarea-addon',
+      props.grouped && 'is-grouped',
+      props.horizontal && 'is-horizontal',
     ]"
   >
-    <slot></slot>
+    <template v-if="typeof props.label === 'string' && props.horizontal">
+      <div class="field-label is-normal">
+        <label class="label">{{ props.label }}</label>
+      </div>
+      <div class="field-body">
+        <slot></slot>
+      </div>
+    </template>
+    <template v-else-if="typeof props.label === 'string'">
+      <label>
+        {{ props.label }}
+      </label>
+
+      <slot></slot>
+    </template>
+    <template v-else>
+      <slot></slot>
+    </template>
   </div>
 </template>
