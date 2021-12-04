e8b1132f2555a357b121a9253b1b43653f7cbae5
Sacha STAFYNIAK <sacha@digisquad.io>
2021-12-04 16:32:00 +0100

    fix: simplify app.ts + main.ts files and create VueroApp.vue

---
+++ src/VueroApp.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+import { useApi } from '/@src/composable/useApi'
+import { useNotyf } from '/@src/composable/useNotyf'
+import { useUserSession } from '/@src/stores/userSession'
+
+/**
+ * Here you can check if your user has a token stored
+ * and check with your api if it still valid before your app start
+ */
+const userSession = useUserSession()
+const api = useApi()
+const router = useRouter()
+const notyf = useNotyf()
+
+onMounted(async () => {
+  if (userSession.isLoggedIn) {
+    try {
+      // do api request call to retreive user profile
+      // note that the api is provided with json-server
+      const { data: user } = await api.get('/api/users/me')
+      userSession.setUser(user)
+      notyf.success(`Welcome back, ${user.name}`)
+    } catch (err) {
+      // delete stored token if it fails
+      userSession.logoutUser()
+      notyf.error('Your token is invalid')
+      // redirect the user somewhere
+      router.push('/auth')
+    }
+  }
+})
+</script>
+
+<template>
+  <RouterView v-slot="{ Component }">
+    <transition name="fade-slow" mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </RouterView>
+  <VReloadPrompt app-name="Vuero" />
+</template>
--- src/main.ts
+++ src/main.ts
@@ -10,39 +10,94 @@
  * @see /index.html
  * @see /vite.config.ts
  */
-
+import { App } from 'vue'
+import { Router } from 'vue-router'
 import { createApp } from './app'
+import { useNotyf } from './composable/useNotyf'
+import { useUserSession } from './stores/userSession'
+
+// Lazy load aditional components
+async function registerGlobalComponents(app: App) {
+  const VCalendar = (await import('v-calendar')).default
+  const VueMultiselect = (await import('@vueform/multiselect')).default
+  const VueSlider = (await import('@vueform/slider')).default
+  const VueTippy = (await import('vue-tippy')).default
+
+  const background = (await import('./directives/background')).default
+  const tooltip = (await import('./directives/tooltip')).default
+
+  app.use(VCalendar)
+  app.use(VueTippy, {
+    defaultProps: {
+      theme: 'light',
+    },
+  })
+
+  app.component(VueMultiselect.name, VueMultiselect)
+  app.component(VueSlider.name, VueSlider)
+
+  app.directive('background', background)
+  app.directive('tooltip', tooltip)
+}
+
+/**
+ * Here we are setting up router navigation guards
+ *
+ * We can add meta to pages either by declaring them manualy in the
+ * routes declaration (see /@src/router.ts)
+ * or by adding a <route> tag into .vue files (see /@src/pages/sidebar/dashboards.ts)
+ *
+ * <route lang="yaml">
+ * meta:
+ *   requiresAuth: true
+ * </route>
+ *
+ * <script setup lang="ts">
+ *  // TS script
+ * </script>
+ *
+ * <template>
+ *  // HTML content
+ * </template>
+ */
+function registerRouterNavigationGuards(router: Router) {
+  router.beforeEach((to, from) => {
+    const userSession = useUserSession()
+
+    if (to.meta.requiresAuth && !userSession.isLoggedIn) {
+      // this route requires auth, check if logged in
+      // if not, redirect to login page.
+      const notif = useNotyf()
+      notif.error({
+        message: 'Sorry, you should loggin to access this section (anything will work)',
+        duration: 7000,
+      })
+
+      return {
+        // Will follow the redirection set in /@src/pages/auth/index.vue
+        name: 'auth',
+        // save the location we were at to come back later
+        query: { redirect: to.fullPath },
+      }
+    }
+  })
+}
 
 /**
  * We create our app and mount it when it is ready
  *
  * @see /@src/app.ts for more detailed informations
  */
-createApp({
-  async enhanceApp(app) {
-    // Lazy load aditional components
-    const VCalendar = (await import('v-calendar')).default
-    const VueMultiselect = (await import('@vueform/multiselect')).default
-    const VueSlider = (await import('@vueform/slider')).default
-    const VueTippy = (await import('vue-tippy')).default
+createApp().then(async ({ app, router }) => {
+  // register router middleware
+  registerRouterNavigationGuards(router)
 
-    const background = (await import('./directives/background')).default
-    const tooltip = (await import('./directives/tooltip')).default
+  // register global components
+  await registerGlobalComponents(app)
 
-    app.use(VCalendar)
-    app.use(VueTippy, {
-      defaultProps: {
-        theme: 'light',
-      },
-    })
-
-    app.component(VueMultiselect.name, VueMultiselect)
-    app.component(VueSlider.name, VueSlider)
-
-    app.directive('background', background)
-    app.directive('tooltip', tooltip)
-  },
-}).then(async ({ app, router }) => {
+  // wait for the app to be ready
   await router.isReady()
+
+  // finaly mount the app to the DOM
   app.mount('#app')
 })
