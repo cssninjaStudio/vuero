918dae5ebf0592c1ebebb2990a28c3a15037ee0b
Sacha STAFYNIAK <sacha@digisquad.io>
2022-07-23 00:38:41 +0200

    feat(router): add scroll behavior between routes

--- src/router.ts
+++ src/router.ts
@@ -44,6 +44,47 @@ export function createRouter() {
      */
     // history: createWebHistory('my-subdirectory'),
     history: createWebHistory(),
+
+    // handle scroll behavior between routes
+    scrollBehavior: (to, _, savedPosition) => {
+      // Scroll to heading on click
+      if (to.hash) {
+        if (to.hash === '#') {
+          return {
+            top: 0,
+            behavior: 'smooth',
+          }
+        }
+
+        const el = document.querySelector(to.hash)
+
+        // vue-router does not incorporate scroll-margin-top on its own.
+        if (el) {
+          const top = parseFloat(getComputedStyle(el).scrollMarginTop)
+          if (el instanceof HTMLElement) {
+            el.focus()
+          }
+
+          return {
+            el: to.hash,
+            behavior: 'smooth',
+            top,
+          }
+        }
+
+        return {
+          el: to.hash,
+          behavior: 'smooth',
+        }
+      }
+
+      // Scroll to top of window
+      if (savedPosition) {
+        return savedPosition
+      } else {
+        return { top: 0 }
+      }
+    },
   })
 
   return router
