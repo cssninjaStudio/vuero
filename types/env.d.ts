91448dad942f8ac60574721e651ef898ae6d11ec
Sacha STAFYNIAK <sacha@digisquad.io>
2023-05-07 05:17:57 +0200

    chore: move .env to .env.example

--- types/env.d.ts
+++ types/env.d.ts
@@ -4,8 +4,8 @@
  */
 
 interface ImportMetaEnv extends Readonly<Record<string, string>> {
-  readonly VITE_API_BASE_URL: string
-  readonly VITE_MAPBOX_ACCESS_TOKEN: string
+  readonly VITE_API_BASE_URL: string | undefined
+  readonly VITE_MAPBOX_ACCESS_TOKEN: string | undefined
 }
 
 interface ImportMeta {
