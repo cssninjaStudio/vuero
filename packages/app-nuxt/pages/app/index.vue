034f8875a6439c75ed17c1b3dafd26768e5a2800
Sacha STAFYNIAK <sacha@digisquad.io>
2024-05-07 01:33:01 +0200

    feat: add Nuxt quickstarter

---
+++ packages/app-nuxt/pages/app/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const pageTitle = useState('page-title')
+onMounted(() => {
+  pageTitle.value = 'My App'
+})
+</script>
+
+<template>
+  <div>
+    <VCard>
+      Hello Vuero
+    </VCard>
+  </div>
+</template>
